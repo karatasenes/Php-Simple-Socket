f4cf12fd310f061f5cd390eba5276fa46ada0952
Enes Karataş <eneskaratas503@gmail.com>
2023-07-24 07:28:54 +0300

    Update defines.php

--- Lib/defines.php
+++ Lib/defines.php
@@ -5,5 +5,7 @@ interface SocketFace
 {
     public function setIp(string $ip);
     public function setPort(int $port);
+    public function createServer(): void;
+    public function  responseSocket(): void;
     public  function request(string $request) : object;
 }
